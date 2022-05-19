7bcc2e0d5ed38c1a1fd03ff3d261e2fcd7ce4ce3
VMMARIN <vanessam.marin@gmail.com>
2022-05-19 10:36:54 -0700

    homepush

--- app/javascript/components/components/Header.js
+++ app/javascript/components/components/Header.js
@@ -1,5 +1,10 @@
 import React, { Component } from 'react'
-import { Nav, NavItem } from 'reactstrap'
+import { Nav, NavItem, Navbar } from 'reactstrap'
+import {Helmet} from 'react-helmet';
+import logo from '../assets/logo.png'
+
+
+
 
 class Header extends Component {
   render() {
@@ -14,25 +19,55 @@ class Header extends Component {
     console.log("current_user:", current_user)
     return (
       <>
-        <h1>React in Rails with Devise</h1>
-        <Nav>
-          {logged_in &&
-            <NavItem>
-              <a href={sign_out_route} className="nav-link">Sign Out</a>
-            </NavItem>
-          }
-          {!logged_in &&
-            <NavItem>
-              <a href={sign_in_route} className="nav-link">Sign In</a>
-            </NavItem>
-          }
-          {!logged_in &&
-            <NavItem>
-              <a href={new_user_route} className="nav-link">Sign Up</a>
-            </NavItem>
-          }
-        </Nav>
-      </>
+      {/* <h1>Apartment App</h1> */}
+      <Nav>
+        {logged_in &&
+          <NavItem>
+            <a href={sign_out_route} className="nav-link">Sign Out</a>
+          </NavItem>
+        }
+        {!logged_in &&
+          <NavItem>
+            <a href={sign_in_route} className="nav-link">Sign In</a>
+          </NavItem>
+        }
+        {!logged_in &&
+          <NavItem>
+            <a href={new_user_route} className="nav-link">Sign Up</a>
+          </NavItem>
+        }
+      </Nav>
+
+    
+
+      <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <a class="navbar-brand" href="/"><img className= "d-sm-block w-50 mx-auto"src={logo}/></a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav mr-auto">
+            <li class="nav-item active">
+              <a class="nav-link" href="/apartmentindex">See All Apartments <span class="sr-only"></span></a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="/apartmentnew">Upload an Apartment Listing</a>
+            </li>
+          </ul>
+        </div>
+      </nav> 
+   
+
+
+      <div>
+        <Helmet>
+          <style>{'body { background-color:#c47a9e; }'}</style>
+        </Helmet>
+
+      </div>
+    
+    </>
     )
   }
 }
--- app/javascript/components/pages/Home.js
+++ app/javascript/components/pages/Home.js
@@ -1,9 +1,21 @@
 import React, { Component } from 'react'
+import {Helmet} from 'react-helmet';
+import logo from '../assets/logo.png'
+
 
 export default class Home extends Component {
   render() {
+    console.log(logo)
     return (
-      <h3>Home</h3>
-    )
-  }
-}
+      <>
+  
+     
+
+      <Helmet>
+        <style>{'body { background-color:#c47a9e; }'}</style>
+      </Helmet>
+      </>
+     )
+     }
+ }
+  
