4436174698c59e812a6fc02fb851d24d60804789
VMMARIN <vanessam.marin@gmail.com>
2022-05-20 13:19:03 -0700

    multiplechanges

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -1,36 +1,186 @@
-import React, { Component } from 'react'
+import React, { Component} from 'react'
 import Header from './components/Header'
-import Footer from './components/Footer'
 import Home from './pages/Home'
 import ApartmentIndex from './pages/ApartmentIndex'
 import ApartmentShow from './pages/ApartmentShow'
+import MyApartmentIndex from './pages/MyApartmentIndex'
 import ApartmentNew from './pages/ApartmentNew'
 import ApartmentEdit from './pages/ApartmentEdit'
 import NotFound from './pages/NotFound'
+
 import {
   BrowserRouter as Router,
   Route,
   Switch
 } from 'react-router-dom'
 
+
 class App extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      apartments: []
+    }
+  }
+
+  componentDidMount(){
+    this.indexApartment()
+  }
+
+  indexApartment = () => {
+    fetch("/apartments")
+    .then(response => {
+      return response.json()
+    })
+    .then(payload => {
+      this.setState({ apartments: payload })
+    })
+    .catch(errors => {
+      console.log("index errors:", errors)
+    })
+  }
+
+  createNewApartment = (newApartment) => {
+    fetch("/apartments", {
+      body: JSON.stringify(newApartment),
+      headers: {
+        "Content-Type": "application/json"
+      },
+      method: "POST"
+    })
+    .then(response => {
+      if(response.status === 422){
+        alert("There is something wrong with your submission.")
+      }
+      return response.json()
+    })
+    .then(() => {
+      this.indexApartment()
+    })
+    .catch(errors => {
+      console.log("create errors:", errors)
+    })
+  }
+
+  editApartment = (apartment, id) => {
+    fetch(`/apartments/${id}`, {
+      body: JSON.stringify(apartment),
+      headers: {
+        "Content-Type": "application/json"
+      },
+      method: "PATCH"
+    })
+    .then(response => {
+      console.log(response)
+      if(response.status === 422){
+        alert("There is something wrong with your submission.")
+      }
+      return response.json()
+    })
+    .then(() => {
+      this.indexApartment()
+    })
+    .catch(errors => {
+      console.log("edit errors:", errors)
+    })
+  }
+
+  deleteApartment = (id) => {
+    alert("Delete this listing?")
+    fetch(`/apartments/${id}`, {
+      headers: {
+        "Content-Type": "application/json"
+      },
+      method: "DELETE"
+    })
+    .then(response => {
+      if(response.status === 422){
+        alert("There is something wrong with your submission.")
+      }
+      return response.json()
+    })
+    .then(() => {
+      this.indexApartment()
+    })
+    .catch(errors => {
+      console.log("delete errors:", errors)
+    })
+  }
+
   render() {
+    const {
+      logged_in,
+      current_user,
+      sign_in_route,
+      sign_out_route,
+      sign_up_route
+    } = this.props
+    const { apartments } = this.state
+
     return (
-      
-        <Router>
-          <Header {...this.props} />
-          <Switch>
-            <Route exact path="/" component={Home} />
-            <Route path="/apartmentindex" component={ApartmentIndex} />
-            <Route path="/apartmentshow" component={ApartmentShow} />
-            <Route path="/apartmentnew" component={ApartmentNew} />
-            <Route path="/apartmentedit" component={ApartmentEdit} />
-            <Route component={NotFound}/>
-          </Switch>
-        </Router>
-        
-        
-  
+      <Router>
+        <Header
+          logged_in={ logged_in }
+          sign_in_route={ sign_in_route }
+          sign_out_route={ sign_out_route }
+        />
+        <Switch>
+          <Route exact path="/" component={ Home } />
+          <Route
+            path="/apartmentindex"
+            render={ (props) => <ApartmentIndex apartments={ apartments } /> }
+          />
+          <Route
+            path="/apartment/:id"
+            render={ (props) => {
+              let id = +props.match.params.id
+              let apartment = apartments.find(a => a.id === id)
+              return <ApartmentShow apartment={ apartment } />
+            }}
+          />
+          { logged_in &&
+            <Route
+              path="/myapartments"
+              render={ (props) => {
+                let myApartments = apartments.filter(apartment => apartment.user_id === current_user.id)
+                return (
+                  <MyApartmentIndex
+                    myApartments={ myApartments }
+                    deleteApartment={ this.deleteApartment }
+                  />
+                )
+              }}
+            />
+          }
+          { logged_in &&
+            <Route
+              path="/new"
+              render={ (props) =>
+                <ApartmentNew
+                  createNewApartment={ this.createNewApartment }
+                  current_user={ current_user }
+                />
+              }
+            />
+          }
+          { logged_in &&
+            <Route
+              path="/apartment-edit/:id"
+              render={ (props) => {
+                let apartment = apartments.find(apartment => apartment.id === +props.match.params.id)
+                return (
+                  <ApartmentEdit
+                    editApartment={ this.editApartment }
+                    current_user={ current_user }
+                    apartment={ apartment }
+                  />
+                )
+              }}
+            />
+          }
+          <Route component={ NotFound } />
+        </Switch>
+      </Router>
     )
   }
 }
--- app/javascript/components/components/Header.js
+++ app/javascript/components/components/Header.js
@@ -1,76 +1,155 @@
+// import React, { Component } from 'react'
+// import { Nav, NavItem, Navbar } from 'reactstrap'
+// import {Helmet} from 'react-helmet';
+// import logo from '../assets/logo.png'
+
+
+
+
+// class Header extends Component {
+//   render() {
+//     const {
+//       logged_in,
+//       current_user,
+//       new_user_route,
+//       sign_in_route,
+//       sign_out_route
+//     } = this.props
+//     console.log("logged_in:", logged_in)
+//     console.log("current_user:", current_user)
+//     return (
+//       <>
+//       {/* <h1>Apartment App</h1> */}
+//       <Nav>
+//         {logged_in &&
+//           <NavItem>
+//             <a href={sign_out_route} className="nav-link">Sign Out</a>
+//           </NavItem>
+//         }
+//         {!logged_in &&
+//           <NavItem>
+//             <a href={sign_in_route} className="nav-link">Sign In</a>
+//           </NavItem>
+//         }
+//         {!logged_in &&
+//           <NavItem>
+//             <a href={new_user_route} className="nav-link">Sign Up</a>
+//           </NavItem>
+//         }
+//       </Nav>
+
+    
+
+//       <nav class="navbar navbar-expand-lg navbar-light bg-light">
+//         <a class="navbar-brand" href="/"><img className= "d-sm-block w-50 mx-auto"src={logo}/></a>
+//         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+//           <span class="navbar-toggler-icon"></span>
+//         </button>
+
+//         <div class="collapse navbar-collapse" id="navbarSupportedContent">
+//           <ul class="navbar-nav mr-auto">
+//             <li class="nav-item active">
+//               <a class="nav-link" href="/apartmentindex">See All Apartments <span class="sr-only"></span></a>
+//             </li>
+//             <li class="nav-item">
+//               <a class="nav-link" href="/apartmentnew">Upload an Apartment Listing</a>
+//             </li>
+//             <li class="nav-item">
+//               <a class="nav-link" href="/">Home</a>
+//             </li>
+//           </ul>
+//         </div>
+//       </nav> 
+   
+
+
+//       <div>
+//         <Helmet>
+//           <style>{'body { background-color:#c47a9e; }'}</style>
+//         </Helmet>
+
+//       </div>
+    
+//     </>
+//     )
+//   }
+// }
+// export default Header
+
+
+
+
+
+
+
+
 import React, { Component } from 'react'
-import { Nav, NavItem, Navbar } from 'reactstrap'
-import {Helmet} from 'react-helmet';
+// import { Nav, NavItem } from 'reactstrap'
+ import { Nav, NavItem, Navbar } from 'reactstrap'
+// import {Helmet} from 'react-helmet';
+import { NavLink } from 'react-router-dom'
 import logo from '../assets/logo.png'
-
-
-
 
 class Header extends Component {
   render() {
     const {
       logged_in,
-      current_user,
-      new_user_route,
       sign_in_route,
       sign_out_route
     } = this.props
-    console.log("logged_in:", logged_in)
-    console.log("current_user:", current_user)
     return (
-      <>
-      {/* <h1>Apartment App</h1> */}
-      <Nav>
-        {logged_in &&
-          <NavItem>
-            <a href={sign_out_route} className="nav-link">Sign Out</a>
-          </NavItem>
-        }
-        {!logged_in &&
-          <NavItem>
-            <a href={sign_in_route} className="nav-link">Sign In</a>
-          </NavItem>
-        }
-        {!logged_in &&
-          <NavItem>
-            <a href={new_user_route} className="nav-link">Sign Up</a>
-          </NavItem>
-        }
-      </Nav>
 
-    
 
       <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <a class="navbar-brand" href="/"><img className= "d-sm-block w-50 mx-auto"src={logo}/></a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
+              <a class="navbar-brand" href="/"><img className= "d-sm-block w-50 mx-auto"src={logo}/></a>
+               <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+      
+       <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav mr-auto">
+       <li class="nav-item active">
 
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul class="navbar-nav mr-auto">
-            <li class="nav-item active">
-              <a class="nav-link" href="/apartmentindex">See All Apartments <span class="sr-only"></span></a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="/apartmentnew">Upload an Apartment Listing</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="/">Home</a>
-            </li>
-          </ul>
-        </div>
-      </nav> 
-   
+       </li>
+      </ul>
+    </div>
+ 
 
 
-      <div>
-        <Helmet>
-          <style>{'body { background-color:#c47a9e; }'}</style>
-        </Helmet>
 
+
+      <div className="header-main">
+        <NavLink to="/"> </NavLink>
+        <Nav>
+          <NavItem>
+            <NavLink to="/">Home</NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink to="/apartmentindex">All the Apartments</NavLink>
+          </NavItem>
+          { logged_in &&
+            <>
+              <NavItem>
+                <NavLink to="/myapartments">My Apartments</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink to="/new">Add Apartment</NavLink>
+              </NavItem>
+              <NavItem>
+                <a href={ sign_out_route }>Sign Out</a>
+              </NavItem>
+            </>
+          }
+          { !logged_in &&
+            <NavItem>
+              <a href={ sign_in_route }>Sign In</a>
+            </NavItem>
+          }
+        </Nav>
       </div>
-    
-    </>
+         </nav>  
+
+         
     )
   }
 }
